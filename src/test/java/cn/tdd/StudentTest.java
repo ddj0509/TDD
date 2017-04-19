f33b0749abd1be9418c243a8b4995ceefe4d5b5a
dongdejun <dejun_dong@163.com>
2017-04-19 14:00:26 +0800

    change classname

---
+++ src/test/java/cn/tdd/StudentTest.java
@@ -0,0 +1,18 @@
+package test.java.cn.tdd;
+
+import main.java.cn.tdd.Student;
+import junit.framework.TestCase;
+
+public class StudentTest extends TestCase {
+  public void testCreate() {
+    final String firstName = "ddj";
+    Student student = new Student(firstName);
+    assertEquals(firstName, student.getName());
+  
+    final String secondName = "lz";
+    Student student2 = new Student(secondName);
+    assertEquals(secondName, student2.getName());
+  }
+
+
+}
--- src/test/java/cn/tdd/testStudent.java
+++
@@ -1,8 +0,0 @@
-package test.java.cn.tdd;
-
-import junit.framework.TestCase;
-
-public class testStudent extends TestCase{
-
-  
-}

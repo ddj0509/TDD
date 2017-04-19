5413be80493e9dda32e0ca02b775e4fbd7d11623
dongdejun <dejun_dong@163.com>
2017-04-19 14:19:02 +0800

    Format

--- src/test/java/cn/tdd/AllTests.java
+++ src/test/java/cn/tdd/AllTests.java
@@ -3,13 +3,13 @@ package test.java.cn.tdd;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class AllTests extends TestCase{
+public class AllTests extends TestCase {
 
   public static TestSuite suite() {
     TestSuite testSuite = new TestSuite();
     testSuite.addTestSuite(StudentTest.class);
     testSuite.addTestSuite(CourseSessionTest.class);
-    
+
     return testSuite;
   }
 }

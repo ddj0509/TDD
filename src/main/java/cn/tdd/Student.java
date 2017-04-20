b51e044619e4a321b47b378535bbe8e2feb6f215
ddj <dejun_dong@163.com>
2017-04-20 23:11:49 +0800

    P181

--- src/main/java/cn/tdd/Student.java
+++ src/main/java/cn/tdd/Student.java
@@ -1,12 +1,26 @@
 package cn.tdd;
 
+import java.util.ArrayList;
+
 public class Student {
   private String name;
   private int credits;
   static final int CREDITS_REQUIRED_FOR_FULL_TIME = 12;
   static final String IN_STATE = "CO";
   private String state;
-  
+  private ArrayList<Grade> grades = new ArrayList<>();
+  static final double GRADE_TOLERANCE = 5 / 100;
+  private boolean isHonors = false;
+
+  enum Grade {
+    A, B, C, D, F
+  };
+
+
+  public void setHonors() {
+    this.isHonors = true;
+  }
+
   public Student(String name) {
     this.name = name;
     this.credits = 0;
@@ -44,8 +58,53 @@ public class Student {
   public void setState(String state) {
     this.state = state;
   }
- 
-  public boolean isInState(){
+
+  public boolean isInState() {
     return Student.IN_STATE.equals(this.state.toUpperCase());
+  }
+
+  public int gradePointsFor(Grade grade) {
+    int result = 0;
+    if(isHonors){
+      if (grade == Student.Grade.A) {
+        result = 5;
+      } else if (grade == Student.Grade.B) {
+        result = 4;
+      } else if (grade == Student.Grade.C) {
+        result = 3;
+      } else if (grade == Student.Grade.D) {
+        result = 2;
+      }
+    }
+    else{
+      if (grade == Student.Grade.A) {
+        result = 4;
+      } else if (grade == Student.Grade.B) {
+        result = 3;
+      } else if (grade == Student.Grade.C) {
+        result = 2;
+      } else if (grade == Student.Grade.D) {
+        result = 1;
+      }
+    }
+    
+    return result;
+  }
+
+  public double getGpa() {
+    if (grades.size() == 0) {
+      return 0;
+    }
+
+    double total = 0.0;
+    for (Grade grade : grades) {
+      total += gradePointsFor(grade);
+    }
+
+    return total / grades.size();
+  }
+
+  public void addGrade(Grade grade) {
+    grades.add(grade);
   }
 }
--- src/test/java/cn/tdd/StudentTest.java
+++ src/test/java/cn/tdd/StudentTest.java
@@ -1,6 +1,7 @@
 package cn.tdd;
 
 import cn.tdd.Student;
+import cn.tdd.Student.Grade;
 import junit.framework.TestCase;
 
 public class StudentTest extends TestCase {
@@ -18,7 +19,7 @@ public class StudentTest extends TestCase {
   public void testFullTime() {
     Student student = new Student("a");
     assertFalse(student.isFullTime());
-    
+
     student.addCredits(20);
     assertTrue(student.isFullTime());
   }
@@ -26,29 +27,70 @@ public class StudentTest extends TestCase {
   public void testStudentStatus() {
     Student student = new Student("a");
     assertEquals(0, student.getCredits());
-    
+
     assertFalse(student.isFullTime());
-    
+
     student.addCredits(3);
     assertEquals(3, student.getCredits());
     assertFalse(student.isFullTime());
-    
+
     student.addCredits(4);
     assertEquals(7, student.getCredits());
     assertFalse(student.isFullTime());
-    
+
     student.addCredits(5);
     assertEquals(Student.CREDITS_REQUIRED_FOR_FULL_TIME, student.getCredits());
     assertTrue(student.isFullTime());
   }
-  
+
   public void testInState() {
     Student student = new Student("a");
     assertFalse(student.isInState());
     student.setState(Student.IN_STATE);
     assertTrue(student.isInState());
-    
+
     student.setState("MD");
     assertFalse(student.isInState());
+  }
+
+  public void testCalculateGpa() {
+    Student student = new Student("a");
+    assertGpa(student, 0.0);
+
+    student.addGrade(Student.Grade.A);
+    assertGpa(student, 4.0);
+
+    student.addGrade(Student.Grade.B);
+    assertGpa(student, 3.5);
+
+    student.addGrade(Student.Grade.C);
+    assertGpa(student, 3.0);
+
+    student.addGrade(Student.Grade.D);
+    assertGpa(student, 2.5);
+  }
+
+  public void testCalculateHonorsStudentGpa() {
+    Student student = new Student("a");
+    assertGpa(createHonorsStudent(student), 0.0);
+    assertGpa(createHonorsStudent(student, Student.Grade.A), 5.0);
+    assertGpa(createHonorsStudent(student, Student.Grade.B), 4.5);
+    assertGpa(createHonorsStudent(student, Student.Grade.C), 4.0);
+    assertGpa(createHonorsStudent(student, Student.Grade.D), 3.5);
+  }
+
+  private Student createHonorsStudent(Student student, Grade grade) {
+    student.addGrade(grade);
+    student.setHonors();
+    return student;
+  }
+
+  void assertGpa(Student student, double expectedGpa) {
+    assertEquals(expectedGpa, student.getGpa(), Student.GRADE_TOLERANCE);
+  }
+
+  Student createHonorsStudent(Student student) {
+    student.setHonors();
+    return student;
   }
 }
